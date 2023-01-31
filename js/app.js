b0f88d5666bf0d4df9261047e280b6c4b2db880d
sel1ght <97527140+sel1ght@users.noreply.github.com>
2023-01-31 17:45:11 +0300

    horiPScroll

---
+++ js/app.js
@@ -0,0 +1,11 @@
+new Swiper('.slider', {
+    speed: 2400,
+    // direction: 'vertical',
+    // freeMode: true,
+    mousewheel: {
+        enabled: true,
+        sensitivity: 2.4
+    },
+    spaceBetween: 18,
+    parallax: true,
+})
